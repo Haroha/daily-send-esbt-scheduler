4ff8745eee32fe38d025c3bd13a9397d025a6d9f
Roger Luo <roger2200@gmail.com>
2021-06-03 00:41:43 +0800

    Fix reg exp no return str bug

--- send_esbt.py
+++ send_esbt.py
@@ -99,7 +99,7 @@ def main(args):
         browser.get(URL)
 
         title = browser.find_element(By.CLASS_NAME, 'title-text')
-        version = re.search('ver\. \d+', title.text)
+        version = re.search('ver\. \d+', title.text).group()
         logger.info("version: %s" % version)
         if version is not "ver. 20210601":
             raise RuntimeError("Version incompatible")
