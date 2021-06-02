9f0df0dd0b2650337e3cc4cab9f148690b44fc05
Roger Luo <roger2200@gmail.com>
2021-06-02 22:58:44 +0800

    Fix str is not callable bug

--- send_esbt.py
+++ send_esbt.py
@@ -99,9 +99,9 @@ def main(args):
         browser.get(URL)
 
         title = browser.find_element(By.CLASS_NAME('title-text'))
-        version = re.search('ver\. \d+', title.getText())
+        version = re.search('ver\. \d+', title)
         logger.info("version: %s" % version)
-        if version != "ver. 20210601":
+        if version is not "ver. 20210601":
             raise RuntimeError("Version incompatible")
 
         sessions = browser.find_elements_by_xpath(
